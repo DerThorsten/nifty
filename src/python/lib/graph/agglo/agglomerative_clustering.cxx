6c4889a881574d4a947378b4f5a94c1a44b37929
DerThorsten <thorsten.beier@iwr.uni-heidelberg.de>
2016-07-30 23:44:58 +0200

    inital version of agglo clustering

--- src/python/lib/graph/agglo/agglomerative_clustering.cxx
+++ src/python/lib/graph/agglo/agglomerative_clustering.cxx
@@ -36,7 +36,7 @@ namespace agglo{
 
         // cls
         py::class_<AgglomerativeClusteringType>(aggloModule, aggloClsName.c_str())
-            .def("run",&AgglomerativeClusteringType::run)
+            .def("run",&AgglomerativeClusteringType::run,"run clustering")
         ;
 
 
