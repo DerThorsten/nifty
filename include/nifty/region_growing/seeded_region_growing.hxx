505e288c2a09c01de61251754e783a38981a048d
DerThorsten <thorsten.beier@iwr.uni-heidelberg.de>
2016-09-07 19:14:18 +0200

    minor changes

---
+++ include/nifty/region_growing/seeded_region_growing.hxx
@@ -0,0 +1,190 @@
+// THIS FILE IS A MODICTION OF 
+// https://github.com/bjoern-andres/seeded-region-growing/blob/master/include/andres/vision/seeded-region-growing.hxx
+
+// Seeded region growing in n-dimensional grid graphs, in linear time.
+//
+// Copyright (c) 2013 by Bjoern Andres.
+// 
+// This software was developed by Bjoern Andres.
+// Enquiries shall be directed to bjoern@andres.sc.
+//
+// All advertising materials mentioning features or use of this software must
+// display the following acknowledgement: ``This product includes andres::vision 
+// developed by Bjoern Andres. Please direct enquiries concerning andres::vision 
+// to bjoern@andres.sc''.
+//
+// Redistribution and use in source and binary forms, with or without 
+// modification, are permitted provided that the following conditions are met:
+//
+// - Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+// - Redistributions in binary form must reproduce the above copyright notice, 
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+// - All advertising materials mentioning features or use of this software must 
+//   display the following acknowledgement: ``This product includes 
+//   andres::vision developed by Bjoern Andres. Please direct enquiries 
+//   concerning andres::vision to bjoern@andres.sc''.
+// - The name of the author must not be used to endorse or promote products 
+//   derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED 
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO 
+// EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+// OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+#pragma once
+#ifndef NIFTY_REGION_GROWING_SEEDED_REGION_GROWING_HXX
+#define NIFTY_REGION_GROWING_SEEDED_REGION_GROWING_HXX
+
+#include <vector>
+#include <queue>
+#include <algorithm> // max
+#include <map>
+
+#include "nifty/marray/marray.hxx"
+//#include "andres/vision/connected-components.hxx"
+
+
+namespace nifty {
+namespace region_growing {
+
+namespace detail {
+   template<class T> 
+      inline bool isAtSeedBorder(const marray::View<T>& seeds, const size_t index);
+}
+
+
+/// Seeded region growing in an n-dimension array, using the 2n-neighborhood
+///
+/// This function operates in-place on its second parameter.
+///
+/// \param elevation 8-bit Elevation map 
+/// \param seeds As input: labeled seeds; as output: labeled grown regions
+///
+template<class INTEGRAL_PIXEL_TYPE,  class T>
+void 
+seededRegionGrowing(
+    const marray::View<INTEGRAL_PIXEL_TYPE> & elevation,
+    marray::View<T>& seeds,
+    const size_t numberOfQueues = 256
+) {
+
+    if(elevation.dimension() != seeds.dimension()) {
+        throw std::runtime_error("dimension of elevation and seeds mismatch.");
+    }
+    for(size_t j = 0; j < elevation.dimension(); ++j) {
+        if(elevation.shape(j) != seeds.shape(j)) {
+            throw std::runtime_error("shape of elevation and seeds mismatch.");
+        }
+    }
+
+    // define 256 queues, one for each gray level.
+    std::vector<std::queue<size_t> > queues(numberOfQueues);
+
+    // add each unlabeled pixels which is adjacent to a seed
+    // to the queue corresponding to its gray level
+    for(size_t j = 0; j < seeds.size(); ++j) {
+        if(detail::isAtSeedBorder<T>(seeds, j)) {
+            NIFTY_CHECK_OP(elevation(j),<,numberOfQueues, "wrong data in evaluation map\n");
+            queues[elevation(j)].push(j);
+        }
+    }
+
+    // grow
+    INTEGRAL_PIXEL_TYPE grayLevel = 0;
+    std::vector<size_t> coordinate(elevation.dimension());
+    for(;;) {
+        while(!queues[grayLevel].empty()) {
+            // label pixel and remove from queue
+            size_t j = queues[grayLevel].front();
+            queues[grayLevel].pop();
+
+            // add unlabeled neighbors to queues
+            seeds.indexToCoordinates(j, coordinate.begin());
+            for(auto d = 0; d < elevation.dimension(); ++d) {
+                if(coordinate[d] != 0) {
+                    --coordinate[d];
+                    if(seeds(coordinate.begin()) == 0) {
+                        size_t k;
+                        seeds.coordinatesToIndex(coordinate.begin(), k);
+                        INTEGRAL_PIXEL_TYPE queueIndex = std::max(elevation(coordinate.begin()), grayLevel);
+                        seeds(k) = seeds(j); // label pixel
+                        queues[queueIndex].push(k);
+                    }
+                    ++coordinate[d];
+                }
+            }
+            for(auto d = 0; d < elevation.dimension(); ++d) {
+                if(coordinate[d] < seeds.shape(d) - 1) {
+                    ++coordinate[d];
+                    if(seeds(coordinate.begin()) == 0) {
+                        size_t k;
+                        seeds.coordinatesToIndex(coordinate.begin(), k);
+                        INTEGRAL_PIXEL_TYPE queueIndex = std::max(elevation(coordinate.begin()), grayLevel);
+                        seeds(k) = seeds(j); // label pixel
+                        queues[queueIndex].push(k);
+                    }
+                    --coordinate[d];
+                }
+            }
+        }
+        if(grayLevel == numberOfQueues-1) {
+            break;
+        }
+        else {
+            queues[grayLevel] = std::queue<size_t>(); // free memory
+            ++grayLevel;
+        }
+    }
+}
+
+// \cond SUPPRESS_DOXYGEN
+namespace detail {
+
+template<class T>
+inline bool isAtSeedBorder(
+    const marray::View<T>& seeds,
+    const size_t index
+) {
+    if(seeds(index) == 0) { 
+        return false; // not a seed voxel
+    }
+    else {
+        std::vector<size_t> coordinate(seeds.dimension());
+        seeds.indexToCoordinates(index, coordinate.begin());
+        for(unsigned char d = 0; d < seeds.dimension(); ++d) {
+            if(coordinate[d] != 0) {
+                --coordinate[d];
+                if(seeds(coordinate.begin()) == 0) {
+                    return true;
+                }
+                ++coordinate[d];
+            }
+        }
+        for(unsigned char d = 0; d < seeds.dimension(); ++d) {
+            if(coordinate[d] < seeds.shape(d) - 1) {
+                ++coordinate[d];
+                if(seeds(coordinate.begin()) == 0) {
+                    return true;
+                }
+                --coordinate[d];
+            }
+        }
+        return false;
+    }
+}
+
+} // namespace detail
+// \endcond 
+
+} // namespace region_growing
+} // namespace nifty
+
+#endif // #ifndef NIFTY_REGION_GROWING_SEEDED_REGION_GROWING_HXX
---
+++ src/python/lib/ground_truth/ground_truth.cxx
@@ -0,0 +1,37 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
+#include <iostream>
+
+#include "nifty/python/converter.hxx"
+
+#include "nifty/graph/undirected_list_graph.hxx"
+
+namespace py = pybind11;
+
+namespace nifty{
+namespace ground_truth{
+
+    void exportPostProcessCarvingNeuroGroundTruth(py::module & groundTruthModule);
+
+}
+}
+
+
+
+
+
+
+
+
+PYBIND11_PLUGIN(_ground_truth) {
+    py::module groundTruthModule("_ground_truth", "ground truth submodule of nifty python bindings");
+
+    using namespace nifty::ground_truth;
+
+        
+    exportPostProcessCarvingNeuroGroundTruth(groundTruthModule);
+    
+
+        
+    return groundTruthModule.ptr();
+}
