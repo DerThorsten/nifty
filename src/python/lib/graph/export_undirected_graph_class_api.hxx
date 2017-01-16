604fcd2434439d87d5cc743c7062c52a7382f682
DerThorsten <thorsten.beier@iwr.uni-heidelberg.de>
2017-01-16 12:56:51 +0100

    chaged caster

--- src/python/lib/graph/export_undirected_graph_class_api.hxx
+++ src/python/lib/graph/export_undirected_graph_class_api.hxx
@@ -2,10 +2,72 @@
 #ifndef NIFTY_PYTHON_GRAPH_EXPORT_UNDIRECTED_GRAPH_CLASS_API_HXX
 #define NIFTY_PYTHON_GRAPH_EXPORT_UNDIRECTED_GRAPH_CLASS_API_HXX
 
+#include <pybind11/pybind11.h>
+#include <pybind11/cast.h>
+
 #include "nifty/python/converter.hxx"
-#include "nifty/python/graph/adjacency_converter.hxx"
 
 namespace py = pybind11;
+
+
+
+
+
+
+
+namespace pybind11 {
+namespace detail {
+
+template <typename T1, typename T2> class type_caster<nifty::graph::detail_graph::UndirectedAdjacency<T1, T2>> {
+    typedef nifty::graph::detail_graph::UndirectedAdjacency<T1, T2> type;
+public:
+    bool load(handle src, bool convert) {
+        if (!isinstance<sequence>(src))
+            return false;
+        const auto seq = reinterpret_borrow<sequence>(src);
+        if (seq.size() != 2)
+            return false;
+        return first.load(seq[0], convert) && 
+               second.load(seq[1], convert);
+    }
+
+    static handle cast(const type &src, return_value_policy policy, handle parent) {
+        auto o1 = reinterpret_steal<object>(make_caster<T1>::cast(src.node(), policy, parent));
+        auto o2 = reinterpret_steal<object>(make_caster<T2>::cast(src.edge(), policy, parent));
+        if (!o1 || !o2)
+            return handle();
+        tuple result(2);
+        PyTuple_SET_ITEM(result.ptr(), 0, o1.release().ptr());
+        PyTuple_SET_ITEM(result.ptr(), 1, o2.release().ptr());
+        return result.release();
+    }
+
+    static PYBIND11_DESCR name() {
+        return type_descr(
+            _("Adjacency[") + make_caster<T1>::name() + _(", ") + make_caster<T2>::name() + _("]")
+        );
+    }
+
+    template <typename T> using cast_op_type = type;
+
+    operator type() {
+        return type(cast_op<T1>(first), cast_op<T2>(second));
+    }
+protected:
+    make_caster<T1> first;
+    make_caster<T2> second;
+};
+
+}
+}
+
+
+
+
+
+
+
+
 
 
 namespace nifty{
