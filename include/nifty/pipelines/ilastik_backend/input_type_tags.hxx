5cd03ce12cfdf65451142d3d705d6c681cf6fb5b
DerThorsten <thorsten.beier@iwr.uni-heidelberg.de>
2017-01-23 18:18:58 +0100

    is working

--- include/nifty/pipelines/ilastik_backend/input_type_tags.hxx
+++ include/nifty/pipelines/ilastik_backend/input_type_tags.hxx
@@ -7,10 +7,14 @@ namespace ilastik_backend{
 
     template<size_t DIM>
     class SpatialTag{
+    public:
+        typedef std::integral_constant<size_t , DIM> DimensionType;
     };
 
     template<size_t DIM>
     class SpatialWithTimeTag{
+    public:
+        typedef std::integral_constant<size_t , DIM> DimensionType;
     };  
 
 
