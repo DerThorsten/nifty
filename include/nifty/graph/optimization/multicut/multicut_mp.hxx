fcde5c044ef4193da1f7500fa51431759ff94c8a
Constantin Pape <c.pape@gmx.net>
2017-05-07 14:29:31 +0200

    lp mp can be called with multicut factory (not properly exposed to python yey)

--- include/nifty/graph/optimization/multicut/multicut_mp.hxx
+++ include/nifty/graph/optimization/multicut/multicut_mp.hxx
@@ -33,6 +33,7 @@ namespace graph{
         typedef MulticutFactoryBase<Objective> McFactoryBase;
 
     public:
+        
         // TODO this should just accept a mulituct factory
         // then we can choose any nifty solver at runtime
         // for now hard-code to greedy additive (doesn't need complicated params...)
@@ -41,7 +42,7 @@ namespace graph{
             
             typedef Graph GraphType;
 
-            NiftyRounder()
+            NiftyRounder(std::shared_ptr<McFactoryBase> factory) : factory_(factory)
             {}
 
             // TODO do we have to call by value here due to using async or could we also use a call by refernce?
@@ -57,7 +58,7 @@ namespace graph{
                         objWeights[eId] = edgeValues[eId];
                     }
                    
-                    auto solverPtr = settings_.mcFactory_->createRawPtr(obj);
+                    auto solverPtr = factory_->createRawPtr(obj);
                     NodeLabels nodeLabeling(g.numberOfNodes());
                     solverPtr->optimize(nodeLabeling, nullptr);
                     delete solverPtr;
@@ -78,14 +79,16 @@ namespace graph{
                 return "NiftyRounder";
             }
 
-        //private:
-        //    std::shared_ptr<McFactoryBase> factory_;
+        private:
+            std::shared_ptr<McFactoryBase> factory_;
         };
         
         // TODO with or without odd wheel ?
-        //typedef LP_MP::FMC_MULTICUT<LP_MP::MessageSendingType::SRMP> FMC;
-        typedef LP_MP::FMC_ODD_WHEEL_MULTICUT<LP_MP::MessageSendingType::SRMP,NiftyRounder> FMC;
-        typedef LP_MP::ProblemConstructorRoundingSolver<LP_MP::Solver<FMC,LP_MP::LP,LP_MP::StandardTighteningVisitor>> SolverType;
+        typedef LP_MP::FMC_MULTICUT<LP_MP::MessageSendingType::SRMP,NiftyRounder> FMC;
+        //typedef LP_MP::FMC_ODD_WHEEL_MULTICUT<LP_MP::MessageSendingType::SRMP,NiftyRounder> FMC;
+        
+        typedef LP_MP::Solver<FMC,LP_MP::LP,LP_MP::StandardTighteningVisitor,NiftyRounder> SolverBase;
+        typedef LP_MP::ProblemConstructorRoundingSolver<SolverBase> SolverType;
 
         // FIXME verbose deosn't have any effect right now
         struct Settings{
@@ -163,7 +166,7 @@ namespace graph{
             typedef MulticutFactory<DefaultSolver> DefaultFactory;
             settings_.mcFactory = std::make_shared<DefaultFactory>();
         }
-        mpSolver_ = new SolverType( toOptionsVector() );
+        mpSolver_ = new SolverType( toOptionsVector(), NiftyRounder(settings_.mcFactory) );
         this->initializeMp();
     }
 
@@ -190,47 +193,34 @@ namespace graph{
     std::vector<std::string> MulticutMp<OBJECTIVE>::
     toOptionsVector() const {
 
-        //std::vector<std::string> options = {
-        //  "export_multicut", // TODO name of pyfile
-        //  "-i", " ", // empty input file
-        //  "--primalComputationInterval", std::to_string(settings_.primalComputationInterval),
-        //  "--standardReparametrization", settings_.standardReparametrization,
-        //  "--roundingReparametrization", settings_.roundingReparametrization,
-        //  "--tightenReparametrization",  settings_.tightenReparametrization,
-        //  "--tightenInterval",           std::to_string(settings_.tightenInterval),
-        //  "--tightenIteration",          std::to_string(settings_.tightenIteration),
-        //  "--tightenSlope",              std::to_string(settings_.tightenSlope),
-        //  "--tightenConstraintsPercentage", std::to_string(settings_.tightenConstraintsPercentage),
-        //  "--maxIter", std::to_string(settings_.numberOfIterations),
-        //};
-        //if(settings_.tighten)
-        //    options.push_back("--tighten");
-        //if(settings_.minDualImprovement > 0) {
-        //    options.push_back("--minDualImprovement");
-        //    options.push_back(std::to_string(settings_.minDualImprovement));
-        //}
-        //if(settings_.minDualImprovementInterval > 0) {
-        //    options.push_back("--minDualImprovementInterval");
-        //    options.push_back(std::to_string(settings_.minDualImprovementInterval));
-        //}
-        //if(settings_.timeout > 0) {
-        //    options.push_back("--timeout");
-        //    options.push_back(std::to_string(settings_.timeout));
-        //}
-        
         std::vector<std::string> options = {
-            "export_multicut",
-            "-i", "",
-            "--tighten",
-            "--tightenReparametrization", "damped_uniform",
-            "--roundingReparametrization", "damped_uniform",
-            "--tightenIteration", "10",
-            "--tightenInterval", "100",
-            "--tightenSlope", "0.02",
-            "--tightenConstraintsPercentage", "0.1",
-            "--primalComputationInterval", "100",
-            "--maxIter", "1000"
+          "export_multicut", // TODO name of pyfile
+          "-i", " ", // empty input file
+          "--primalComputationInterval", std::to_string(settings_.primalComputationInterval),
+          "--standardReparametrization", settings_.standardReparametrization,
+          "--roundingReparametrization", settings_.roundingReparametrization,
+          "--tightenReparametrization",  settings_.tightenReparametrization,
+          "--tightenInterval",           std::to_string(settings_.tightenInterval),
+          "--tightenIteration",          std::to_string(settings_.tightenIteration),
+          "--tightenSlope",              std::to_string(settings_.tightenSlope),
+          "--tightenConstraintsPercentage", std::to_string(settings_.tightenConstraintsPercentage),
+          "--maxIter", std::to_string(settings_.numberOfIterations),
         };
+        if(settings_.tighten)
+            options.push_back("--tighten");
+        if(settings_.minDualImprovement > 0) {
+            options.push_back("--minDualImprovement");
+            options.push_back(std::to_string(settings_.minDualImprovement));
+        }
+        if(settings_.minDualImprovementInterval > 0) {
+            options.push_back("--minDualImprovementInterval");
+            options.push_back(std::to_string(settings_.minDualImprovementInterval));
+        }
+        if(settings_.timeout > 0) {
+            options.push_back("--timeout");
+            options.push_back(std::to_string(settings_.timeout));
+        }
+        
         return options;
     }
 
