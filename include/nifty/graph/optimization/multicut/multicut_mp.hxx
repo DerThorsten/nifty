0b1b83cdd423b1b3736d4319ca834d50d835dbdf
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2017-05-11 00:23:58 +0200

    Fix missing include

--- include/nifty/graph/optimization/multicut/multicut_mp.hxx
+++ include/nifty/graph/optimization/multicut/multicut_mp.hxx
@@ -5,7 +5,7 @@
 #include "nifty/tools/runtime_check.hxx"
 #include "nifty/graph/optimization/multicut/multicut_base.hxx"
 #include "nifty/graph/optimization/multicut/multicut_factory.hxx"
-//#include "nifty/graph/optimization/multicut/multicut_greedy_additive.hxx"
+#include "nifty/graph/optimization/multicut/multicut_greedy_additive.hxx"
 //#include "nifty/graph/optimization/multicut/multicut_kernighan_lin.hxx"
 #include "nifty/graph/optimization/multicut/multicut_andres.hxx"
 #include "nifty/ufd/ufd.hxx"
