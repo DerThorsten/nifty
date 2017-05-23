92d836f98ed4be96550335caa5fd5bea47afeb88
DerThorsten <thorsten.beier@iwr.uni-heidelberg.de>
2017-05-23 22:00:54 +0200

    added header

---
+++ include/nifty/python/graph/undirected_grid_graph.hxx
@@ -0,0 +1,30 @@
+#pragma once
+#include <string>
+
+#include "nifty/python/graph/graph_name.hxx"
+#include "nifty/graph/undirected_grid_graph.hxx"
+
+
+namespace nifty{
+namespace graph{
+
+
+
+    template<
+        size_t DIM,
+        bool SIMPLE_NH
+    >
+    struct GraphName<UndirectedGridGraph<DIM, SIMPLE_NH>>{
+        static std::string name(){
+            return std::string("UndirectedGridGraph") + 
+                std::to_string(DIM) +
+                std::string("D") + 
+                (SIMPLE_NH ? 
+                    std::string("SimpleNh") : 
+                    std::string("ExtendedNh"));
+                
+        }
+    };
+
+}
+}
