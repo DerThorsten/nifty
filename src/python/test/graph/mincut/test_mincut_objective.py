d490df21a9fb1d76621798165d73b4529d44f667
ahmadnish <neishabouri@stud.uni-heidelberg.de>
2017-07-20 11:43:46 +0200

    running python-test

--- src/python/test/graph/mincut/test_mincut_objective.py
+++ src/python/test/graph/mincut/test_mincut_objective.py
@@ -37,13 +37,56 @@ class TestMincutObjective(unittest.TestCase):
 
     def testGridModel(self, gridSize = [5,5],  weightRange = [-2,1]):
 
+        beta = .7
+        gridSize = [5,5]  
+        weightRange = [0,1]
+
         g,nid = self.generateGrid(gridSize)
-        d = weightRange[1] - weightRange[0]
-        w = numpy.random.rand(g.numberOfEdges)*d + float(weightRange[0])
-        print(w.min(),w.max())
-        obj = nifty.graph.mincut.mincutObjective(g,w)
+
+        # d = weightRange[1] - weightRange[0]
+        # w = numpy.random.rand(g.numberOfEdges)*d + float(weightRange[0])
+        # print(w.min(),w.max())
+
+        w = numpy.zeros(g.numberOfEdges)
+        
         labels = numpy.zeros(g.nodeIdUpperBound+1,dtype='uint8')
+
+
+        for x in range(gridSize[0]):
+            for y in range(gridSize[1]):
+
+                nodeId = nid(x,y)
+                if x in range(1,gridSize[0] - 1):
+                    if y in range(1,gridSize[1] - 1):
+                        labels[nodeId] = 1
+                    else:
+                        labels[nodeId] = 0
+                else:
+                    labels[nodeId] = 0
+
+        for x in range(gridSize[0]):
+            for y in range(gridSize[1]):
+
+                p = nid(x,y)
+
+                if x + 1 < gridSize[0]:
+                    q = nid(x+1,y)
+                    weightId = g.findEdge(p,q)
+                    if (labels[p] == labels[q]):
+                        w[weightId] = 0
+                    else:
+                        w[weightId] = beta
+                if y + 1 < gridSize[1]:
+                    q = nid(x, y + 1)
+                    weightId = g.findEdge(p,q)
+                    if (labels[p] == labels[q]):
+                        w[weightId] = 0
+                    else:
+                        w[weightId] = beta
+
+        obj = nifty.graph.mincut.mincutObjective(g,w)
+
         value = obj.evalNodeLabels(labels)
-        self.assertAlmostEqual(value,0.0)
+        self.assertAlmostEqual(value,8.4)
         return obj
 
