d49b2491df21e1777c66d0cf7a8d010eb920dda9
DerThorsten <thorsten.beier@iwr.uni-heidelberg.de>
2017-12-01 18:04:37 +0100

    added stuff

---
+++ include/nifty/python/graph/undirected_long_range_grid_graph.hxx
@@ -0,0 +1,35 @@
+#pragma once
+#include <string>
+
+#include "nifty/python/graph/graph_name.hxx"
+#include "nifty/graph/undirected_long_range_grid_graph.hxx"
+
+
+namespace nifty{
+namespace graph{
+
+
+
+    template<
+        size_t DIM
+    >
+    struct GraphName<UndirectedLongRangeGridGraph<DIM>>{
+        static std::string name(){
+            return std::string("UndirectedLongRangeGridGraph") + 
+                std::to_string(DIM) +
+                std::string("D");
+        }
+
+        static std::string moduleName(){
+            return std::string("nifty.graph");
+        }
+
+        static std::string usageExample(){
+            return std::string(
+                "import nifty\n"
+            );
+        }
+    };
+
+}
+}
