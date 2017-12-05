73d4ce02f3461c7852ab210901ce2b60d3d7b1f3
Constantin Pape <c.pape@gmx.net>
2017-12-04 22:59:49 -0500

    Starting z5 support

---
+++ include/nifty/z5/z5.hxx
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "xtensor/xarray.hpp"
+#include "z5/dataset.hxx"
+#include "z5/multiarray/xt_access.hxx"
+#include "nifty/tools/block_access.hxx"
+
+namespace nifty{
+namespace tools{
+
+    template<class T, class COORD>
+    inline void readSubarray(const z5::Dataset & ds,
+                             const COORD & beginCoord,
+                             const COORD & endCoord,
+                             xt::xarray<T> & subarray){
+        z5::multiarray::readSubarray<T>(ds, subarray, beginCoord.begin());
+    }
+
+    template<class T, class COORD>
+    inline void writeSubarray(z5::Dataset & ds,
+                              const COORD & beginCoord,
+                              const COORD & endCoord,
+                              const xt::xarray<T> & subarray){
+        z5::multiarray::writeSubarray<T>(ds, subarray, beginCoord.begin());
+    }
+
+    template<class ARRAY>
+    struct BlockStorageSelector;
+
+    struct BlockStorageSelector<z5::Dataset>
+    {
+       typedef BlockStorage<T> type;
+    };
+
+} // namespace nifty::tools
+} // namespace nifty
