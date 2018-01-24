ce9d09883b62174d5317d6fc34e4ea2b4f7f30f7
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2018-01-24 16:29:39 -0500

    Add tool to map labeling to np array

--- src/python/lib/tools/map_dict.cxx
+++ src/python/lib/tools/map_dict.cxx
@@ -24,22 +24,31 @@ namespace tools{
             mapDictionaryToArray<DIM>(data, dict, haveIgnoreValue, ignoreValue);
 
         }, py::arg("data"), py::arg("dict"), py::arg("haveIgnoreValue")=false, py::arg("ignoreValue")=0);
+
+
+        toolsModule.def("mapLabelingToArray",
+        [](xt::pytensor<T, DIM> & data, xt::pytensor<T, 1> labeling, bool haveIgnoreValue, T ignoreValue){
+
+            py::gil_scoped_release allowThreads;
+            mapLabelingToArray<DIM>(data, labeling, haveIgnoreValue, ignoreValue);
+
+        }, py::arg("data"), py::arg("labeling"), py::arg("haveIgnoreValue")=false, py::arg("ignoreValue")=0);
     }
 
 
     void exportMapDictionaryToArray(py::module & toolsModule) {
 
         exportMapDictionaryToArrayT<uint32_t, 2>(toolsModule);
-        //exportMapDictionaryToArrayT<uint32_t, 3>(toolsModule);
+        exportMapDictionaryToArrayT<uint32_t, 3>(toolsModule);
 
-        //exportMapDictionaryToArrayT<uint64_t, 2>(toolsModule);
-        //exportMapDictionaryToArrayT<uint64_t, 3>(toolsModule);
+        exportMapDictionaryToArrayT<uint64_t, 2>(toolsModule);
+        exportMapDictionaryToArrayT<uint64_t, 3>(toolsModule);
 
-        //exportMapDictionaryToArrayT<int32_t, 2>(toolsModule);
-        //exportMapDictionaryToArrayT<int32_t, 3>(toolsModule);
+        exportMapDictionaryToArrayT<int32_t, 2>(toolsModule);
+        exportMapDictionaryToArrayT<int32_t, 3>(toolsModule);
 
-        //exportMapDictionaryToArrayT<int64_t, 2>(toolsModule);
-        //exportMapDictionaryToArrayT<int64_t, 3>(toolsModule);
+        exportMapDictionaryToArrayT<int64_t, 2>(toolsModule);
+        exportMapDictionaryToArrayT<int64_t, 3>(toolsModule);
     }
 
 }
