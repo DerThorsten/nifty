321134325a3fdb6c6dda1113b72a0ea6a70690c9
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2018-02-01 16:53:27 -0500

    Skeleton for feature extraction

---
+++ include/nifty/distributed/features.hxx
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "nifty/distributed/distributed_graph.hxx"
+
+namespace nifty {
+namespace distributed {
+
+
+    // TODO support different feature extractors with common backend
+    inline void extractBlockFeatures(const std::string & groupPath,
+                                     const std::string & blockPrefix,
+                                     const std::vector<size_t> & blockIds,
+                                     const std::vector & outGroup) {
+
+    }
+
+}
+}
