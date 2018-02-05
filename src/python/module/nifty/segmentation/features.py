aebde116821cbe444f51f2d5c797994eb6e929b2
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2018-02-05 11:12:50 -0500

    Minor work on features

--- src/python/module/nifty/segmentation/features.py
+++ src/python/module/nifty/segmentation/features.py
@@ -3,9 +3,14 @@ from .base import ProblemExtractor
 
 
 # TODO
+STAT_TO_INDEX = {'mean': 0}
+
+
+# TODO
 class MeanAffinitiyMapFeatures(ProblemExtractor):
-    def __init__(self, statisric='mean'):
-        pass
+    def __init__(self, statistic='mean'):
+        assert statistic in STAT_TO_INDEX
+        self.stat_index = STAT_TO_INDEX[statistic]
 
     def _compute_edge_probabilities(self, input_, fragments=None):
         assert input_.ndim == 4
@@ -14,7 +19,8 @@ class MeanAffinitiyMapFeatures(ProblemExtractor):
 # TODO
 class MeanBoundaryMapFeatures(ProblemExtractor):
     def __init__(self, statistic='mean'):
-        pass
+        assert statistic in STAT_TO_INDEX
+        self.stat_index = STAT_TO_INDEX[statistic]
 
     def _compute_edge_probabilities(self, input_, fragments=None):
         assert input_.ndim == 3
