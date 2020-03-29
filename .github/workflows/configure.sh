58eca3f3c279a506b046984ecbc4e6f354c994f4
Constantin Pape <c.pape@gmx.net>
2020-03-29 15:13:48 +0200

    Use system compiler instead of conda compiler

--- .github/workflows/configure.sh
+++ .github/workflows/configure.sh
@@ -1,11 +1,11 @@
 # TODO how do we set the python version properly?
-conda create -q -n dev -c conda-forge -c defaults cmake gcc_linux-64 gxx_linux-64
+conda create -q -n dev -c conda-forge python=3.7 cmake  # gcc_linux-64 gxx_linux-64
 source activate dev
 conda install -c conda-forge xtensor-python boost-cpp scikit-image h5py vigra z5py>=1.5.1 nlohmann_json
         
 export ENV_BIN="$CONDA_PREFIX/bin"
-export CC="$ENV_BIN/x86_64-conda_cos6-linux-gnu-cc"
-export CXX="$ENV_BIN/x86_64-conda_cos6-linux-gnu-c++"
+# export CC="$ENV_BIN/x86_64-conda_cos6-linux-gnu-cc"
+# export CXX="$ENV_BIN/x86_64-conda_cos6-linux-gnu-c++"
         
 $ENV_BIN/cmake \
     -DWITHIN_TRAVIS=ON \
