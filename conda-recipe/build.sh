167715dc21849d2c9ce5361d6cf53018c63c38ae
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2020-04-22 20:21:16 +0200

    Update conda recipe

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,24 +1,25 @@
-# export CC=x86_64-conda_cos6-linux-gnu-gcc
-# export CXX=x86_64-conda_cos6-linux-gnu-g++
+# export CC="${PREFIX}/bin/x86_64-conda_cos6-linux-gnu-gcc"
+# export CXX="${PREFIX}/bin/x86_64-conda_cos6-linux-gnu-g++"
+export CC=x86_64-conda_cos6-linux-gnu-gcc
+export CXX=x86_64-conda_cos6-linux-gnu-g++
 export PY_INSTALL_DIR="${PREFIX}/lib/python3.7/site-packages"
 
 ##
 ## START THE BUILD
 ##
 
-mkdir -p build
-cd build
-
 CXXFLAGS="${CXXFLAGS} -I${PREFIX}/include -std=c++17"
 
 ##
 ## Configure
 ##
-cmake .. \
+cmake . \
         -DCMAKE_BUILD_TYPE=RELEASE \
         -DCMAKE_PREFIX_PATH="${PREFIX}" \
         -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
         -DPYTHON_MODULE_INSTALL_DIR="${PY_INSTALL_DIR}" \
+        -DCMAKE_C_COMPILER="${CC}" \
+        -DCMAKE_CXX_COMPILER="${CXX}" \
 \
         -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
 \
@@ -26,7 +27,7 @@ cmake .. \
         -DWITH_Z5=ON \
         -DWITH_ZLIB=ON \
         -DWITH_BLOSC=ON \
-        -DWITH_BZIP2=ON \
+        -DWITH_BZIP2=OFF \
         -DWITH_BOOST_FS=OFF \
 \
         -DBUILD_NIFTY_PYTHON=ON \
