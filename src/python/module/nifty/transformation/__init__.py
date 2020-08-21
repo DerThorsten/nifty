8370ca1f9cecfbb64737732e94cd831e268b995c
Constantin Pape <c.pape@gmx.net>
2020-08-21 11:09:39 +0200

    Expose new transformation functionality in python

--- src/python/module/nifty/transformation/__init__.py
+++ src/python/module/nifty/transformation/__init__.py
@@ -53,3 +53,18 @@ def affineTransformation(data, matrix, order, bounding_box,
     func = "affineTransformation%iD%s" % (ndim, dtype)
     func = getattr(_trafo_impl, func)
     return func(data, matrix, order, start, stop, fill_value)
+
+
+def coordinateTransformationZ5(data, coordinate_file, bounding_box, fill_value=0):
+    """
+    """
+    ndim = data.ndim
+    start = [bb.start for bb in bounding_box]
+    stop = [bb.stop for bb in bounding_box]
+    if ndim != len(start) != len(stop):
+        raise ValueError("Invalid dimension")
+    dtype = str(data.dtype)
+    func = "coordinateTransformationZ5%iD%s" % (ndim, dtype)
+    func = getattr(_trafo_impl, func)
+    path, key = get_path_and_key_from_dataset(data)
+    return func(path, key, coordinate_file, start, stop, fill_value)
