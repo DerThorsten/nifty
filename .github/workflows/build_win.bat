dc154009242a367fd32407c74ce290c5e2238245
Constantin Pape <c.pape@gmx.net>
2021-03-04 17:37:55 +0100

    Build with z5 and hdf5 in windows ci

--- .github/workflows/build_win.bat
+++ .github/workflows/build_win.bat
@@ -1,18 +1,16 @@
 cmake . -G "NMake Makefiles" ^
     -DWITH_QPBO=OFF ^
     -DWITH_HDF5=OFF ^
-    -DWITH_Z5=OFF ^
     -DWITH_GLPK=OFF ^
     -DWITH_CPLEX=OFF ^
     -DWITH_GUROBI=OFF ^
     -DBUILD_CPP_TEST=OFF ^
     -DCMAKE_PREFIX_PATH="%CONDA_PREFIX%" ^
     -DCMAKE_INSTALL_PREFIX="%CONDA_PREFIX%" ^
-    -DBUILD_NIFTY_PYTHON=ON
-
-REM FIXME z5 tests still fail on windows 
-REM    -DWITH_Z5=ON ^
-REM    -DWITH_ZLIB=ON ^
-REM    -DWITH_BLOSC=ON ^
+    -DBUILD_NIFTY_PYTHON=ON ^
+    -DWITH_HDF5=ON ^
+    -DWITH_Z5=ON ^
+    -DWITH_ZLIB=ON ^
+    -DWITH_BLOSC=ON
 
 cmake --build . --target INSTALL --config Release -j 4
