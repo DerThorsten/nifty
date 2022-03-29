c93a59fe1de6919db025c4f3713cc20c0a43edc0
Constantin Pape <c.pape@gmx.net>
2022-03-29 12:30:12 +0200

    Add test for affinity features

--- src/python/test/graph/rag/test_accumulate.py
+++ src/python/test/graph/rag/test_accumulate.py
@@ -23,6 +23,14 @@ class TestAccumulate(unittest.TestCase):
         res = nrag.accumulateEdgeMeanAndLength(rag, data)
         self.assertTrue(np.sum(res) != 0)
 
+    def test_accumulate_affinities(self):
+        labels = np.random.randint(0, 100, size=self.shape_2d, dtype='uint32')
+        rag = nrag.gridRag(labels, numberOfLabels=100)
+        offsets = [[-1, 0], [0, -1], [-3, 0], [0, -3]]
+        aff_shape = (len(offsets),) + labels.shape
+        data = np.random.random_sample(aff_shape).astype('float32')
+        res = nrag.accumulateAffinityStandartFeatures(rag, data, offsets, 0.0, 1.0)
+        self.assertTrue(np.sum(res) != 0)
 
 
 if __name__ == '__main__':
